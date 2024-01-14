0b70a409e3607f94538101aa737b6f50bf481827
Cocci38 <sandra.villot038@gmail.com>
2024-01-14 11:01:54 +0100

    modal Color

--- portals_react/src/components/ModalColor.jsx
+++ portals_react/src/components/ModalColor.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 
-export default function ModalColor ({ closeModalBis }) {
+export default function ModalColor({ closeModalColor }) {
     return (
         <>
             <div
-                onClick={closeModalBis}
+                onClick={closeModalColor}
                 className=""
             ></div>
-            <div>ModalColor</div>
+            <div className="modalMini">
+                <div className="" style={{backgroundColor:"#A62C31",width:"30px",height:"30px"}}></div>
+                <button
+                    className="button-close"
+                    onClick={closeModalColor}
+                >
+                    X
+                </button>
+            </div>
+
         </>
     )
 }
--- portals_react/src/components/ModalContent.jsx
+++ portals_react/src/components/ModalContent.jsx
@@ -5,6 +5,14 @@ import ModalColor from "./ModalColor";
 
 export default function ModalContent({ closeModal }) {
     const [showModalBis, setShowModalBis] = useState(false);
+    const [showModalColor, setShowModalColor] = useState(false);
+
+    let shadow = false;
+    if (showModalBis) {
+        shadow = true;
+    } else if (showModalColor) {
+        shadow = true;
+    }
     return (
         <>
             <div id="modal-window" class="shadow" ></div>
@@ -12,16 +20,16 @@ export default function ModalContent({ closeModal }) {
                 onClick={closeModal}
                 className=""
             ></div>
-            <div className={!showModalBis ? "modal" : "modal opacity"}>
+            <div className={!shadow ? "modal" : "modal opacity"}>
                 <div className="modal-content"><button className={!showModalBis ? "buttonModal" : "buttonHide"} onClick={() => setShowModalBis(true)}>Mini</button>
                     {showModalBis && createPortal(
                         <ModalMini closeModalBis={() => setShowModalBis(false)} />,
                         document.body
                     )}
                     
-                    <button className={!showModalBis ? "buttonModal" : "buttonHide"} onClick={() => setShowModalBis(true)}>Couleurs</button>
-                    {showModalBis && createPortal(
-                        <ModalColor closeModalBis={() => setShowModalBis(false)} />,
+                    <button className={!showModalColor ? "buttonModal" : "buttonHide"} onClick={() => setShowModalColor(true)}>Couleurs</button>
+                    {showModalColor && createPortal(
+                        <ModalColor closeModalColor={() => setShowModalColor(false)} />,
                         document.body
                     )}</div>
                 <button
