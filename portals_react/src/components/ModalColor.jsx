11604b7aaa90275cc40287874f2eb4a801d1de51
Cocci38 <sandra.villot038@gmail.com>
2024-03-09 20:44:58 +0100

    color change

--- portals_react/src/components/ModalColor.jsx
+++ portals_react/src/components/ModalColor.jsx
@@ -2,39 +2,26 @@ import React, { useState } from 'react'
 
 export default function ModalColor({ closeModalColor }) {
 
-    const [showColors, setShowColors] = useState(false);
-    const colors = [{"id":0,"name":"red","code":"#A62C31"},{"id":1,"name":"blue","code":"#89ADF3"}];
+    const [showColors, setShowColors] = useState("");
+    const colors = [{ "id": 0, "name": "red", "code": "#A62C31" }, { "id": 1, "name": "blue", "code": "#89ADF3" }];
     
-    let background = "";
     const changeColor = (code) => {
-        console.log(code);
-        
-        
-        console.log(background);
-        if (showColors == false) {
-            setShowColors(true);
-        }else {
-            setShowColors(false);
-        }
-        background = code;
-        //return background;
-
+        setShowColors(code);
     }
     //console.log(showColors);
-    console.log(background);
     return (
         <>
             <div
                 onClick={closeModalColor}
             ></div>
-            <div className="modalMini" style={setShowColors ? {background} : ""}>
-                <div style={{backgroundColor: background}} /*style={{width:"50px",height:"50px",backgroundColor:background}}*/></div>
+            <div className="modalMini" >
+                <div style={{ background:showColors, width:"50px",height:"50px"}} /*style={{width:"50px",height:"50px",backgroundColor:background}}*/></div>
                 <div className='flexRow'>
-                {Array.isArray(colors) ? colors 
-                .map((color) => (
-                    <div key={color.id}  className={color.name} onClick={() => { changeColor(color.code) }} style={{backgroundColor:color.code,width:"30px",height:"30px",borderRadius:"10px"}}></div>
-                ))
-                :""}
+                    {Array.isArray(colors) ? colors
+                        .map((color) => (
+                            <div key={color.id} className={color.name} onClick={() => { changeColor(color.code) }} style={{ backgroundColor: color.code, width: "30px", height: "30px", borderRadius: "10px" }}></div>
+                        ))
+                        : ""}
                 </div>
                 <button
                     className="button-close"
